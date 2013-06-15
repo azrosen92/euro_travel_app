c6ee1c5a3951bfb5dcac0d67a788c8ae2636afa9
Aaron Rosen <azrosen92@gmail.com>
2013-06-15 17:55:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EuroTravelApp::Application.config.secret_token = 'a16e0d1d0b237e5b352ebd39f543d808ae3384203b0871294fb248ef995f2f4b97a052b26800c2f2000c072733c55bb43f43a45b346d835df28a77d7a8901e51'
